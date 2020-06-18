dacd117a8ee9d3307fb007f5c06c70a4f8c9b54b
Juyoung Kim <kjyoung153@hotmail.com>
2020-06-18 17:43:07 +0900

    html naver copy2

--- naver/js/naver.js
+++ naver/js/naver.js
@@ -1,3 +1,12 @@
 $(function(){
-    
+    $('html').scrollTop(0);
+    $('.btn_close').click(function(e){
+        $('.naver-whale').remove();
+        //3일동안 보지 않기 버튼을 클릭했을 때 url에 #이 붙는 것을 방지
+        e.preventDefault();
+    })
+    $('.btn-auto-search-fold').click(function(){
+        $('.autoframe').toggleClass('display-none');
+
+    })
 })
