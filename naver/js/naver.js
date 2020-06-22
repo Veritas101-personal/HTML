08908d974bb82d52d06d298bb259b3b1da0266d2
Juyoung Kim <kjyoung153@hotmail.com>
2020-06-22 17:47:44 +0900

    html naver copying 5

--- naver/js/naver.js
+++ naver/js/naver.js
@@ -1,4 +1,10 @@
 $(function(){
+    var defaultMenu = ['사전','뉴스','증권','부동산','지도','영화','뮤직','책','웹툰'];
+    var defaultMenuLink =['https://dict.naver.com','https://news.naver.com','https://finance.naver.com','https://land.naver.com','https://map.naver.com/v5/','https://movie.naver.com','https://vibe.naver.com/about','https://book.naver.com','https://comic.naver.com/index/nhn'];
+    var selectMenu = [];
+    var selectAllLink = [];
+    var allMenuLink = ['모든 링크가 위에처럼 있어야함'];
+
     $('html').scrollTop(0);
     $('.btn_close').click(function(e){
         $('.naver-whale').remove();
@@ -13,13 +19,30 @@ $(function(){
     })
     $('.btn-more').click(function(){
         $('.btn-more').toggleClass('display-none');
-        $('.extra-service').toggleClass('extra-window');
-        $('.box-group-keyword').toggleClass('display-none');        
+        $('.extra-service').toggleClass('display-none');
+        $('.extra-service.display').addClass('display-none');
+        $('.box-service-menu.set').removeClass('display-none');
+        $('.box-group-keyword').toggleClass('display-none');  
         $('.box-service-menu').toggleClass('display-none');   
         
     })
-    $('.btn-menu.set').click(function(){
-        
+    $('.btn-menu-set').click(function(){
+        $('.extra-group').toggleClass('display-none');
+        $('.extra-choice').toggleClass('display-none');
+
+        var cnt=0;
+        var max=5;
+
+        $('.favorite-service').each(function(){
+            if(cnt<max){
+                $(this).addClass('box-empty');
+                $(this).find('.link-menu-black').text('');
+            } else{
+                $(this).addClass('display-none');
+            }
+        })
+
     })
+    $('.')
 })
     
